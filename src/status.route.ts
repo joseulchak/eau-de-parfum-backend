81a7db1932752bf1ba1d796a04a22f470f71b1d9
Jose Ulchak <joseulchak@gmail.com>
2025-03-02 23:28:12 -0300

    test deploy vercel

--- src/status.route.ts
+++ src/status.route.ts
@@ -4,4 +4,8 @@ export default async function statusRoutes(fastify: FastifyInstance) {
     fastify.get('/status', (request: FastifyRequest, reply: FastifyReply) => {
         reply.send({ status: "ok" })
     })
+
+    fastify.get('/', (request: FastifyRequest, reply: FastifyReply) => {
+        reply.send({ status: "Eau de Parfum" })
+    })
 }
