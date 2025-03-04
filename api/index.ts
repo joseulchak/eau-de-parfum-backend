588fd565eec08cffeaa830e316ba2e0cbf76a736
Jose Ulchak <joseulchak@gmail.com>
2025-03-04 01:13:57 -0300

    added mock for authentication by jwt

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,11 @@
 'use strict'
 import express from 'express'
-import routes from './routes'
+import routes from './routes/routes'
 
 const app = express();
-app.use('/', routes)
+app.use(express.json());
+
+app.use(routes)
 app.listen(3000, () => console.log("Server ready on port 3000!."));
 
 export default app
--- api/routes/auth.route.ts
+++ api/routes/auth.route.ts
@@ -1,12 +1,31 @@
 import { BASE_URL } from '../../constants';
-import '../@types/session.type';
 import { Router } from 'express'
+import jwt from 'jsonwebtoken'
 
+const secretKey = 'secretKey'
+const userCredentials = {
+    user: 'geicy',
+    pwd: '123456',
+}
 
 const router = Router()
 
-router.post(`${BASE_URL}/auth`, (req, res) => {
-    res.send('auth endpoint!')
+router.post(`${BASE_URL}/auth`, async(req, res) => {
+    const { username, password } = req.body as { username: string, password: string }
+
+    if (password !== userCredentials.pwd) {
+        res.status(403).json({
+            error: 'invalid credentials'
+        })
+        return;
+    }
+
+    const { pwd, ...userData } = userCredentials
+
+    const token = jwt.sign(userData, secretKey, { expiresIn: '1h' })
+
+    res.cookie('token', token)
+    res.send('authenticated!')
 })
 
 export default router
